0d2db8250455d43e6bedc715f4d8111af20f607e
David Waldron <davidwaldron66@gmail.com>
2020-02-06 12:20:09 -0600

    update function

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-list/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -73,6 +74,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       AlertifyService,
       MemberDetailResolver,
       MemberEditResolver,
+      PreventUnsavedChanges,
       MemberListResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
